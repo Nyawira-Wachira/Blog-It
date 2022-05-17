16b4668863249835f4040b4e90ca4fa34faf865f
ABBY <abbyW@moringa.com>
2022-05-17 17:28:32 +0300

    Made adjustments to app for heroku deployment.

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,8 @@ from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '9aad4163fdb903e924e956c2152ef40a1d44'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///account.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://bwjuhsexzwtzhd:0199895fc234163d0ff6110bee45109652094462ea7131513ce496106c62509d@ec2-34-231-177-125.compute-1.amazonaws.com:5432/d68f3fa1hfjrcn'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///account.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
@@ -41,39 +42,4 @@ from app import main
 
 
 
-
-
-
-# # import bcrypt
-# from flask import Flask
-# # from flask_sqlalchemy import SQLAlchemy
-# # from flask_bcrypt import Bcrypt
-# # from flask_login import LoginManager
-# # import os
-# # from os import path
-# app = Flask(__name__)
-
-# # app.config['SECRET_KEY'] = '9aad4163fdb903e924e956c2152ef40a1d44'
-# # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///account.db'
-# # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# # app.config['MAIL_SERVER']='smtp.gmail.com'
-# # app.config['MAIL_PORT'] = 465
-# # app.config['MAIL_USERNAME'] = 'abigail.nyawira22@gmail.com'
-# # app.config['MAIL_PASSWORD'] = 'WaChIrA07!' 
-# # app.config['MAIL_USE_TLS'] = False
-# # app.config['MAIL_USE_SSL'] = True
-# # db = SQLAlchemy(app)
-# # bcrypt = Bcrypt(app)
-# # login_manager = LoginManager(app)
-# # login_manager.login_view = 'login'
-# # login_manager.login_message_category = 'info'
-
-
-
-# # def create_database(app):
-# #     if not path.exists("app/" + "page.db"):
-# #         db.create_all(app=app)
-# #         print("Created database!")
-
-# from app import main
 
